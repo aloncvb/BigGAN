25c69157b70275f0dd857dd75d0680507febf63d
Alon Harari <206786170@idf.il>
2024-07-06 17:11:02 +0300

    try that

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def test(
     gan.eval()  # set to inference mode
     with torch.no_grad():
         batch_size = 32
-        samples = gan.generate_fake(batch_size, fixed_noise[:batch_size])
+        samples = gan.generate_fake(batch_size)
         torchvision.utils.save_image(
             torchvision.utils.make_grid(samples),
             "./samples/" + filename + "epoch%d.png" % epoch,
