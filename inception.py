bc7cb8345a72653ab769bcd53bae77cfd7c037d2
Alon Harari <206786170@idf.il>
2024-08-31 20:31:58 +0300

    not used

--- inception.py
+++
@@ -1,7 +0,0 @@
-import torch.nn as nn
-from torchvision.models import inception_v3, Inception_V3_Weights
-
-
-def get_inception_model(half=True) -> nn.Module:
-    model = inception_v3(weights=Inception_V3_Weights.IMAGENET1K_V1)
-    return model.eval()
